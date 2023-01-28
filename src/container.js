8e63df9b9d185006a382d72dc5f2a342a25d5f17
AlfonMnz <alfonmusan14@gmail.com>
2023-01-28 01:20:42 +0100

    Adding template api features

--- src/container.js
+++ src/container.js
@@ -2,7 +2,7 @@ const awilix = require('awilix');
 
 const mongoDbHandler = require('./infrastructure/persistance/mongo/db-handler');
 const {logger} = require('@Movie-Rewind/core');
-const {ApplicationError} = require('@Movie-Rewind/core');
+const MUUID = require('uuid-mongodb');
 const container = awilix.createContainer({
 	injectionMode: awilix.InjectionMode.PROXY,
 });
@@ -10,7 +10,7 @@ const container = awilix.createContainer({
 container.register({
 	mongoDbHandler: awilix.asFunction(mongoDbHandler),
 	logger: awilix.asValue(logger),
-	ApplicationError: awilix.asClass(ApplicationError),
+	muuid: awilix.asValue(MUUID),
 });
 
 module.exports = container;
